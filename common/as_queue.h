4df10f8c383ea59a2ce97dd9f1f96e2e2dbc8093
何欣 <shrong.he@gmail.com>
2017-12-28 16:57:12 +0800

    add the as_queue

---
+++ common/as_queue.h
@@ -0,0 +1,109 @@
+#ifndef __AS_MEDIA_KENERL_BUFFER_QUEUE_H__
+#define __AS_MEDIA_KENERL_BUFFER_QUEUE_H__
+#include "as_config.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <signal.h>
+#if AS_APP_OS == AS_OS_WIN32
+#include <windows.h>
+#endif
+#include "as_mutex.h"
+
+typedef struct as_queue_s  as_queue_t;
+
+struct as_queue_s {
+    as_queue_t  *prev;
+    as_queue_t  *next;
+};
+
+
+#define ngx_queue_init(q)                                                     \
+    (q)->prev = q;                                                            \
+    (q)->next = q
+
+
+#define ngx_queue_empty(h)                                                    \
+    (h == (h)->prev)
+
+
+#define ngx_queue_insert_head(h, x)                                           \
+    (x)->next = (h)->next;                                                    \
+    (x)->next->prev = x;                                                      \
+    (x)->prev = h;                                                            \
+    (h)->next = x
+
+
+#define ngx_queue_insert_after   ngx_queue_insert_head
+
+
+#define ngx_queue_insert_tail(h, x)                                           \
+    (x)->prev = (h)->prev;                                                    \
+    (x)->prev->next = x;                                                      \
+    (x)->next = h;                                                            \
+    (h)->prev = x
+
+
+#define ngx_queue_head(h)                                                     \
+    (h)->next
+
+
+#define ngx_queue_last(h)                                                     \
+    (h)->prev
+
+
+#define ngx_queue_sentinel(h)                                                 \
+    (h)
+
+
+#define ngx_queue_next(q)                                                     \
+    (q)->next
+
+
+#define ngx_queue_prev(q)                                                     \
+    (q)->prev
+
+
+#if (NGX_DEBUG)
+
+#define ngx_queue_remove(x)                                                   \
+    (x)->next->prev = (x)->prev;                                              \
+    (x)->prev->next = (x)->next;                                              \
+    (x)->prev = NULL;                                                         \
+    (x)->next = NULL
+
+#else
+
+#define ngx_queue_remove(x)                                                   \
+    (x)->next->prev = (x)->prev;                                              \
+    (x)->prev->next = (x)->next
+
+#endif
+
+
+#define ngx_queue_split(h, q, n)                                              \
+    (n)->prev = (h)->prev;                                                    \
+    (n)->prev->next = n;                                                      \
+    (n)->next = q;                                                            \
+    (h)->prev = (q)->prev;                                                    \
+    (h)->prev->next = h;                                                      \
+    (q)->prev = n;
+
+
+#define ngx_queue_add(h, n)                                                   \
+    (h)->prev->next = (n)->next;                                              \
+    (n)->next->prev = (h)->prev;                                              \
+    (h)->prev = (n)->prev;                                                    \
+    (h)->prev->next = h;
+
+
+#define ngx_queue_data(q, type, link)                                         \
+    (type *) ((u_char *) q - offsetof(type, link))
+
+
+as_queue_t *as_queue_middle(as_queue_t *queue);
+void as_queue_sort(as_queue_t *queue,
+    int32_t (*cmp)(const as_queue_t *, const as_queue_t *));
+
+
+
+#endif /* __AS_MEDIA_KENERL_BUFFER_QUEUE_H__ */
